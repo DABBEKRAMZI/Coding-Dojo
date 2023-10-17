5d81857ca8e956a9ea8dbb5b88bcbe5de0bbc4e6
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-10-17 18:32:51 +0100

    ram

---
+++ Programming Basics/Week 1/Day 1/Core/level1.js
@@ -0,0 +1,27 @@
+// Activity 1 //
+var x=5 ;
+console.log(x);
+
+// Activity 2 // 
+var x=3 ;
+x=x+1 ;
+console.log(x) ;
+
+//Activity 3 //
+var x=3 ;
+x=x+2 ;
+console.log(x+2);
+console.log(x);
+
+//Activity 4 //
+var x=3 ;
+x=x*x ;
+console.log(x);
+console.log(x*2) ;
+
+// Activity 5 //
+var x=2;
+var y=5 ;
+var z=x+y;
+console.log(z) ;
+
