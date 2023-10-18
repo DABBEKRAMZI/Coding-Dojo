273a8a61b4e0fe0b3c3cc5150057f38bd27e7265
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-10-18 11:24:24 +0100

    cc

---
+++ Programming Basics/Week 1/Day 2/Core/conditional.js
@@ -0,0 +1,31 @@
+ // variables : age and height for kid we can change if we need 
+var age = 9 , height=45 ; 
+  
+
+// height condition 
+
+if (height >= 42){
+    console.log("Get on that ride,kiddo!") ;
+}
+else {
+    console.log("Sorry kiddp Maybe next year") ;
+}
+
+// height and age condition 
+
+if (height >= 42 && age >= 10){
+    console.log("Get on that ride,kiddo!") ;
+}
+else {
+    console.log("Sorry kiddp Maybe next year") ;
+}
+// height or age condition 
+
+if (height >= 42 || age >= 10){
+    console.log("Get on that ride,kiddo!") ;
+}
+else {
+    console.log("Sorry kiddp Maybe next year") ;
+}
+
+
