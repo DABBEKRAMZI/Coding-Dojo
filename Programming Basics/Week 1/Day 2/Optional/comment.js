9b733fcf0da49d6b76e45f77dcce4bad23a0f320
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-10-18 09:59:16 +0100

    comm

---
+++ Programming Basics/Week 1/Day 2/Optional/comment.js
@@ -0,0 +1,11 @@
+   //  this is a counter variable //
+var likeCount=0 ;
+
+   /// this function increase the counter variable if we need 
+   function increaseLike () {
+    
+    // with this operation we incremented the old counter value by 1
+    likeCount= likeCount+1 ;
+   } 
+    
+   
