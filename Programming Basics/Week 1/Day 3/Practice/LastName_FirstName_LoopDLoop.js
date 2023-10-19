f7f634bd34b7baffd3448152be0250d8664a7d84
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-10-19 12:50:16 +0100

    commit

---
+++ Programming Basics/Week 1/Day 3/Practice/LastName_FirstName_LoopDLoop.js
@@ -0,0 +1,37 @@
+// Step 1 //
+// How do we know we need a loop here? //
+// we need a loop if we have a task to repeat several times // 
+// What's the starting point of the loop? //
+//  the starting point is the initialisation of loop and This step can only be performed once.//
+// When should the loop stop? //
+// the loop stop when the stop condition is false // 
+// How will the loop know when to stop? // 
+// if the stop condition is not checked // 
+// What's incrementing for each iteration of the loop? // 
+// the counter which we initialized // 
+// What variables do we need? // 
+// a counter // 
+
+// step 2 // 
+
+for ( var i=0 ; i<=6 ; i++){
+    // check the runner reaches 2 miles on a treadmill
+   if ( i%2==0 && i>0){
+
+    console.log(" piece of candy"); 
+   }
+}
+
+// Bonus ninja //
+var speed =10 ;
+
+for ( var i=0 ; i<=6 ; i++){
+  //check reaches 2 miles on a treadmill and his speed faster than 5.5 //
+   if (speed>5.5 ) {
+    // getting a piece of candy //
+    console.log(" piece of candy"); 
+   }
+}
+
+
+
