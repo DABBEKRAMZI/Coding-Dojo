3f1ca8ee30b0c05f394d2b8033ee7d25e20e05ce
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-10-19 13:33:09 +0100

    pedict

---
+++ Programming Basics/Week 1/Day 3/Practice/LastName_FirstName_JSPredict.js
@@ -0,0 +1,37 @@
+// predict 1 // 
+function myBirthYearFunc(){
+    console.log("I was born in "+1980);
+}
+myBirthYearFunc();
+
+// console display  : //
+//  I was born in 1980 //
+
+
+
+// predict 2 //
+function myBirthYearFunc(birthYearInput){
+    console.log("I was born in "+birthYearInput);
+}
+myBirthYearFunc(1980);
+
+// console display  : //
+//  I was born in 1980 //
+
+
+//predict 3 //
+function add(num1,num2){
+    console.log('Summing Numbers');
+    console.log("num1 is ",num1) ;
+    console.log("num2 is ",num2);
+    var sum=num1+num2;
+    console.log(sum);
+
+}
+add(10,20);
+
+// console display: //
+// Summing Numbers  //
+// num1 is   10     //  
+// num1 is   20     //  
+// 30               //
