297417b5e1b4c23de54af9ee5ecb17c539119372
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-10-20 08:53:38 +0100

    param

---
+++ Programming Basics/Week 1/Day 3/Practice/param.js
@@ -0,0 +1,22 @@
+   // step 1
+
+function greet(person){
+    console.log('Good day, ' + person +'!') ;
+}
+greet("ramzi") ;
+
+    // step 2
+function greet1(person,heure){
+    console.log('there is '+heure+' Good day, ' + person +'!') ;
+}
+greet1("ramzi","10am") ;
+
+    // step3
+function greet2(person){
+    if (person=="Count Dooku") console.log("I'm coming for you, Dooku!") ;
+    else console.log('Good day, ' + person +'!') ;
+}
+greet2("Count Dooku") ;
+
+
+
