6539917922ad716ec564cb193717732ebfcded22
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-11-13 21:49:07 +0100

    com

---
+++ Python/Week 1/Day 1/Core/loop.py
@@ -0,0 +1,34 @@
+# chanllenge 1 
+for i in range(0,150,1):
+    print(i)
+
+
+# chanllenge 2 
+for i in range(5,1000,5):
+    print(i)
+
+
+# chanllenge 3
+for i in range(1,101):
+    if(i%10==0): print("Coding Dojo")
+    elif(i%5==0): print("coding")
+    else : print(i)
+
+
+# chanllenge 4
+sum =0
+for i in range(500000):
+    if(i%2==1): sum +=i 
+print(sum)
+
+# chanllenge 5
+for i in range(2018,0,-4):
+    print(i)
+
+# chanllenge 6    
+lowNum=2
+highNum=9
+mult=3
+for i in range(lowNum,highNum+1):
+    if (i%3==0) :print(i)
+
