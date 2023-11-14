6b906f34ccb64ba323c7cf2d9f200578c8a24367
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-11-14 12:46:31 +0100

    last

--- Python/Week 1/Day 1/Core/list_dict.py
+++ Python/Week 1/Day 1/Core/list_dict.py
@@ -40,3 +40,32 @@ iterateDictionary(students)
 # first_name - John, last_name - Rosales
 # first_name - Mark, last_name - Guillen
 # first_name - KB, last_name - Tonel
+
+# challenge 3 
+def iterateDictionary2(key_name, some_list) :
+    list=[]
+    for i in some_list :
+        list.append(i[key_name])
+    for j in list : print(j)
+    return list    
+
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+
+
+# challenge 4 
+def printInfo(some_dict) :
+    for key in some_dict.keys():
+        print( len(some_dict[key]),key)
+        for value in some_dict[key]:
+            print(value)
+
+    return None
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
+
+       
