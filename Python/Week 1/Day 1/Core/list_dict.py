332849d3308f52f5f172b012607910bb052caf97
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-11-14 08:39:09 +0100

    pu

---
+++ Python/Week 1/Day 1/Core/list_dict.py
@@ -0,0 +1,42 @@
+# chalenge 1 
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0]=15
+students[0]['last_name'] ='Bryant'
+sports_directory['soccer'] [0]='Andres'
+z[0]['y']=30
+print(x)
+print(students)
+print(sports_directory)
+print(z)
+
+
+# chalenge 2
+def iterateDictionary(dict):
+    for i in dict :
+           for key in i.keys():
+            print(f"{key} - {i[key]},") 
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+# print(students[0].keys())
+iterateDictionary(students) 
+# # should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# # bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
