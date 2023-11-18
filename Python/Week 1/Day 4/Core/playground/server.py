6b1ef2d94975fe8cfac2dd60c331e9e8317df198
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-11-18 01:39:35 +0100

    yes

---
+++ Python/Week 1/Day 4/Core/playground/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app =Flask(__name__)
+@app.route('/')
+def hi():
+    return f"ahla w sahla"
+
+@app.route('/play')
+def play():
+    return render_template("index.html",number=3)
+
+@app.route('/play/<int:number>')
+def play1(number):
+    return render_template("index.html", number=number)
+
+@app.route('/play/<int:number>/<string:color>')
+def play2(number,color):
+    return render_template("index.html",number=number,color=color)
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
+
+
+
