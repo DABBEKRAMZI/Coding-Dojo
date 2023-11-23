33d02d941f5d35bd5cc087207a2ff1dab0db4b76
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-11-23 22:02:59 +0100

    yes

---
+++ Python/Week 1/Day 5/Core/dojo_survey/server.py
@@ -0,0 +1,24 @@
+from flask import Flask ,render_template , redirect, session,request
+app=Flask(__name__)
+app.secret_key="jm,mk,"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process',methods=['post'])
+def process():
+    session["name"]=request.form["name"]
+    session["dojo_location"]=request.form["dojo_location"]
+    session["Favorite_Language"]=request.form["Favorite_Language"]
+    session["comment"]=request.form["Comment"]
+
+    return redirect('/result')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+
+if __name__=="__main__" :
+    app.run(debug=True,port = 5500)
