56f51ae752878c88beb1f567cee4054515d1e1a3
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-12-21 23:12:30 +0100

    ok

---
+++ MERNStack/MERNStack/week_1/Day_3/Practice/prop_it_up/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './components/PersonCard';
+function App() {
+  const person=[{firs_name:'Jane',last_name:'Doe',age:45,hair_color:'Black'},
+                {firs_name:'John',last_name:'Smith',age:88,hair_color:'Brown'},
+                {firs_name:'Millard',last_name:'Fillmore',age:50,hair_color:'Brown'},
+                {firs_name:'Maria',last_name:'Smith',age:62,hair_color:'Brown'}]
+  return (
+    <div className="App">
+      {person.map((person,idx)=> <PersonCard key={idx} person={person} />)}
+    </div>
+  );
+}
+
+export default App;
---
+++ MERNStack/MERNStack/week_1/Day_3/Practice/prop_it_up/src/components/PersonCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PersonCard =(props)=>{
+    // person: {firs_name: 'Jane', last_name: 'Doe', age: 45, hair_color: 'Black'}
+    console.log(props);
+    return(
+        <>
+            
+            <h1>{props.person.last_name}, {props.person.firs_name}</h1>
+            <p>Age: {props.person.age}</p>
+            <p>Hair color: {props.person.hair_color}</p>
+
+        </>
+    )
+}
+export default PersonCard
