e8b3e8183cce81756fd5771306913be1862bf018
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2023-12-27 13:57:54 +0100

    ok

---
+++ MERNStack/week_1/Day_3/Optional/react_block/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header=()=>{
+const style={
+    backgroundColor:'#6aa84f',
+    width :'100%',
+    height:'100px'
+}
+return( <div style={style}>
+
+</div>);
+
+}
+export default Header;
---
+++ MERNStack/week_1/Day_3/Optional/react_block/src/components/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navigation=()=>{
+    const style={
+        marginTop:'15px',
+        width:"20%",
+        height:'400px',
+        backgroundColor:'#6fa8dc'
+    }
+    return (
+        <div style={style}></div>
+    );
+
+}
+export default Navigation ;
