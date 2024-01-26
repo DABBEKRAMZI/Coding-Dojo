1fce0b384f97acf4dadf4468ad0b6994f93a8ba0
DABBEKRAMZI <dabbekramzi1994@gmail.com>
2024-01-26 14:13:00 +0100

    okok

---
+++ Java/Week1/Day3/Core/Zookeeper/Test.java
@@ -0,0 +1,23 @@
+public class Test {
+    public static void main (String[] args){
+        Gorilla g1=new Gorilla();
+        Bat B1=new Bat();
+        g1.displayEnergy();
+        g1.eatBananas();
+        g1.displayEnergy();
+        g1.throwSomething();
+        g1.displayEnergy();
+        g1.climb();
+        g1.displayEnergy();
+        System.out.println("*****************************");
+        B1.displayEnergy();
+        B1.fly();
+        B1.displayEnergy();
+        B1.eatHumans();
+        B1.displayEnergy();
+        B1.attackTown();
+        B1.displayEnergy();
+
+
+    }
+}
